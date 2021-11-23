b5ce1ef745b566ccdafba2b6e2a1b0950d283a62
xieyang <xieyang@graspyun.com>
2021-11-23 15:15:37 +0800

    init

---
+++ src/components/pages/common/beiAn.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="bottom_beian">
+    <div class="beian">
+      <a target="_blank" href="http://beian.miit.gov.cn/">
+        <img src="@/assets/pic/ghs.png" alt />
+        <p>蜀ICP备2021029095</p>
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  created() {},
+};
+</script>
+<style>
+.bottom_beian {
+  position: fixed;
+  bottom: 0px;
+  text-align: center;
+  width: 100%;
+  background-color: #f3f3f3;
+  line-height: 40px;
+  padding: 5px 0 20px 0;
+}
+.beian p {
+  display: inherit;
+  text-align: center;
+  color: #939393;
+  padding: 0;
+  margin: 0;
+  line-height: 20px;
+}
+.beian a {
+  display: inline-flex;
+  text-align: center;
+}
+</style>
---
+++ src/components/pages/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="content">网站建设中</div>
+    <beiAn></beiAn>
+  </div>
+</template>
+<script>
+import beiAn from "./common/beiAn.vue";
+export default {
+  name: "",
+  props: {},
+  components: { beiAn },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  created() {},
+};
+</script>
+<style>
+</style>
